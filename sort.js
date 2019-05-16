ff1b9904a9b11e513b03da3ee6e2a9315433a6f3
evblance <evblance@users.noreply.github.com>
2019-05-16 14:55:26 +1000

    Initial commit

---
+++ sort.js
@@ -0,0 +1,19 @@
+// Source: https://www.imdb.com/list/ls051421138/
+const MOVIES = [
+  "A Bag of Hammers",
+  "The African Queen",
+  "Beyond Silence",
+  "Not Without My Daughter",
+  "Lucky",
+  "Happy, Texas",
+  "Flowers in the Attic",
+  "Eight Men Out",
+  "Ghost",
+  "Bottle Shock",
+  "Butter",
+  "Airport",
+  "The Man from Earth",
+  "The Quest",
+  "Quills",
+  "The Experts"
+];
