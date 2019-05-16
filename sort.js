09fbaa035a91598476e2b55076848c6ab9493862
evblance <evblance@users.noreply.github.com>
2019-05-16 16:20:02 +1000

    Displays unsorted list to DOM

--- sort.js
+++ sort.js
@@ -17,3 +17,25 @@ const MOVIES = [
   "Quills",
   "The Experts"
 ];
+
+// Store our list item class as a constant
+const LIST_ITEM_CLASSNAME = 'list__item';
+
+// Store our unordered list DOM element reference so we can append to it
+const listNode = document.getElementById('list');
+
+/**
+ * Renders a list of strings to the target unordered list in the DOM.
+ * @param {string[]} list The array of text strings to render. 
+ */
+function renderList(list) {
+  list.forEach((i) => {
+    const item = document.createElement('li');
+    item.classList.add(LIST_ITEM_CLASSNAME);
+    const text = document.createTextNode(i);
+    item.appendChild(text);
+    listNode.appendChild(item);
+  });
+}
+
+renderList(MOVIES);
